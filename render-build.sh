9cc8df2995cd2765174c40daf56289ea868b6d26
Daniel Sotelo <danielescesc@gmail.com>
2025-07-26 08:29:16 -0700

    intento 3

--- render-build.sh
+++ render-build.sh
@@ -2,19 +2,11 @@
 # exit on error
 set -o errexit
 
+# Instala dependencias
 npm install
-# npm run build # descomenta si tu proyecto necesita compilar algo
 
-# Manejo de cache de Puppeteer
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
-  echo "...Copiando caché de Puppeteer desde el caché de compilación" 
-  if [[ -d "$XDG_CACHE_HOME/puppeteer/" ]]; then
-  cp -R "$XDG_CACHE_HOME/puppeteer/" "$PUPPETEER_CACHE_DIR"
-else
-  echo "No se encontró la caché de Puppeteer en $XDG_CACHE_HOME/puppeteer/"
-fi
+# Fuerza instalación de Chromium
+npx puppeteer install
 
-else 
-  echo "...Guardando caché de Puppeteer en el caché de compilación" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
-fi
+# Puedes descomentar esto si tienes una carpeta build
+# npm run build
