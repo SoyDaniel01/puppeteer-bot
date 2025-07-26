6bb138c1b384592fb6a730da21ae2c6f5c16ad1e
Daniel Sotelo <danielescesc@gmail.com>
2025-07-25 19:05:49 -0700

    intento 2

--- render-build.sh
+++ render-build.sh
@@ -8,7 +8,12 @@ npm install
 # Manejo de cache de Puppeteer
 if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
   echo "...Copiando caché de Puppeteer desde el caché de compilación" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  if [[ -d "$XDG_CACHE_HOME/puppeteer/" ]]; then
+  cp -R "$XDG_CACHE_HOME/puppeteer/" "$PUPPETEER_CACHE_DIR"
+else
+  echo "No se encontró la caché de Puppeteer en $XDG_CACHE_HOME/puppeteer/"
+fi
+
 else 
   echo "...Guardando caché de Puppeteer en el caché de compilación" 
   cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
